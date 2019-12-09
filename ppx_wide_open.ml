0f1660372ad64295711530a1f227abcaf0526d53
ziat <ghiles.ziat@gmail.com>
2019-12-09 12:10:08 +0100

    attributes ++; payload 'using'

--- ppx_wide_open.ml
+++ ppx_wide_open.ml
@@ -10,66 +10,74 @@ let remove__ s =
   String.iter (function '_' -> incr nb_occ | _ -> ()) s;
   let s' = Bytes.make (String.length s - !nb_occ) ' ' in
   let nb_cur = ref 0 in
-  String.iteri (fun i c ->
-      if c = '_' then incr nb_cur
-      else Bytes.set s' (i- !nb_cur) c) s;
+  String.iteri (fun i -> function
+      | '_' -> incr nb_cur
+      | c   -> Bytes.set s' (i- !nb_cur) c) s;
   Bytes.to_string s'
 
 (* given an ast fragment representing a string 'c', builds the ast
    fragment for '(parse c)' *)
-let parse_it c loc =
+let parse c fname loc =
   let c = remove__ c in
-  let id = Exp.ident (lid "parse") in
+  let id = Exp.ident (lid fname ) in
   Exp.apply ~loc:loc id [Nolabel,str c]
 
-let replace const mode loc =
-  match const,mode with
-  | Pconst_integer(c,None),(`All | `Ints) -> parse_it c loc
-  | Pconst_float(c,None),(`All | `Floats) -> parse_it c loc
+let replace const name fname loc =
+  match const,name with
+  | Pconst_integer(c,None),("parse.all" | "parse.int")
+  | Pconst_float(c,None),("parse.all" | "parse.float") -> parse c fname loc
   | c,_ -> Exp.constant ~loc c
 
 (* replaces both float and integer litterals [lit] by [parse "lit"]*)
-let parse_mapper_const mode =
+let parse_mapper_const name fname =
   let handle mapper = function
-    | {pexp_desc = Pexp_constant(x); pexp_loc; _ }-> replace x mode pexp_loc
+    | {pexp_desc = Pexp_constant x; pexp_loc;_}-> replace x name fname pexp_loc
     |  x -> default_mapper.expr mapper x
   in
   {default_mapper with expr = handle}
 
-(* search for a given attribute *)
-let find_attribute attrs name =
-  let rec aux ((contains, rest) as acc) = function
-    | [] -> acc
-    | h::tl -> if h.attr_name.txt = name then true,(List.rev rest) @ tl
-               else aux (false,h::rest) tl
-  in
-  aux (false,[]) attrs
+(* can we handle the given attribute? *)
+let check_attr a =
+  match a.attr_name.txt with
+  | "parse.int" | "parse.float" | "parse.all" -> true
+  | _ -> false
 
-(* when a [let open%replace.int M in e] is met,
+(* given the payload of an attribute, computes the name of the
+   function to be used for the parsing of litterals *)
+let get_fname payload loc =
+  match payload with
+  | PStr [] -> "of_string"
+  | PStr [{pstr_desc =
+             Pstr_eval
+               ({pexp_desc =
+                   Pexp_apply
+                     ({pexp_desc = Pexp_ident {txt = Lident "using"};_},
+                      [(Nolabel,{pexp_desc = Pexp_ident{txt=Lident fname};_})]);
+                 _},_)}] -> fname
+  | _ -> Format.printf "%a\n%!" Location.print_loc loc;
+         failwith "wrong payload for parse attribute."
+
+(* when a [let open[parse.int] M in e] is met,
    rewrites [e] using parse_mapper *)
-let replace_mapper _ =
+let expr_mapper _ =
   let handle_expr mapper expr =
     match expr.pexp_desc with
     | Pexp_open(op,exp) ->
-       (try
-          let mode,rest =
-            let res,rest = find_attribute expr.pexp_attributes "replace.int" in
-            if res then `Ints,rest
-            else
-              let res,rest = find_attribute expr.pexp_attributes "replace.float" in
-              if res then `Floats,rest
-              else
-                let res,rest = find_attribute expr.pexp_attributes "replace.all" in
-                if res then `All,rest
-                else raise Exit
-          in
-          let ofs = parse_mapper_const mode in
-          let exp' = ofs.expr ofs exp in
-          let ope = Pexp_open(op,exp') in
-          {expr with pexp_desc = ope; pexp_attributes = rest}
-        with Exit -> expr)
+       (match List.find check_attr expr.pexp_attributes with
+        | attr ->
+           let fname = get_fname attr.attr_payload attr.attr_loc in
+           let ofs = parse_mapper_const attr.attr_name.txt fname in
+           let exp' = ofs.expr ofs exp in
+           let ope = Pexp_open(op,exp') in
+           {expr with pexp_desc = ope;
+                      (* we keep the others potential attributes *
+                       * to not interfere with other PPXs *)
+                      pexp_attributes = List.filter ((<>) attr) expr.pexp_attributes}
+        | exception Not_found -> expr)
     |  _ -> default_mapper.expr mapper expr
   in
   {default_mapper with expr = handle_expr}
 
-let () = register "wide" replace_mapper
+
+
+let () = register "wide" expr_mapper
--- test.ml
+++ test.ml
@@ -1,6 +1,11 @@
 let nb_solutions a b c =
-  let open[@replace.int] Wideopen.Zarith_Z in
-  let delta = b * b - 4 * a * c in
-  if delta > 0 then 2
-  else if delta = 0 then 1
-  else 0
+    let open[@parse.int using of_string] Q in
+    let delta = b * b - 4 * a * c in
+    Format.printf "delta is %s\n" (to_string delta);
+    if delta > 0 then 2
+    else if delta = 0 then 1
+    else 0
+
+let _ =
+  let open[@parse.int using of_string] Q in
+  nb_solutions 1 999999999 1
